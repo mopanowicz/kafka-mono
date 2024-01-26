317da0737d1ad67b7d2eb53565fc2db97cf4876a
mopanowicz <marek.opanowicz@gmail.com>
2024-01-26 18:14:49 +0100

    Initial commit

---
+++ kafka-avro-producer/src/main/java/com/example/kafkaavroproducer/KafkaAvroProducerApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaavroproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaAvroProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaAvroProducerApplication.class, args);
+	}
+
+}
