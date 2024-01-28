74a85341103d5c4e51382921c1f74d9310fe385f
mopanowicz <marek.opanowicz@gmail.com>
2024-01-28 16:05:59 +0100

    Add empty avsc based producer project

---
+++ kafka-avro-producer-avsc/src/main/java/com/example/kafkaavroproduceravsc/KafkaAvroProducerAvscApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaavroproduceravsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaAvroProducerAvscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaAvroProducerAvscApplication.class, args);
+	}
+
+}
---
+++ kafka-avro-producer-avsc/src/test/java/com/example/kafkaavroproduceravsc/KafkaAvroProducerAvscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkaavroproduceravsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaAvroProducerAvscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
