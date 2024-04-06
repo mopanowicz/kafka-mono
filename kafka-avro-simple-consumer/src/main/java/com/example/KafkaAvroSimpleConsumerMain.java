cc395a43dd52b13692d936069a228f33a02cdad1
mopanowicz <marek.opanowicz@gmail.com>
2024-04-06 17:13:15 +0200

    Add skeleton projects

---
+++ kafka-avro-simple-consumer/src/main/java/com/example/KafkaAvroSimpleConsumerMain.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class KafkaAvroSimpleConsumerMain {
+    public static void main(String[] args) {
+        System.out.printf("KafkaAvroSimpleConsumerMain.main");
+    }
+}
---
+++ kafka-avro-simple-producer/src/main/java/com/example/KafkaAvroSimpleProducerMain.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class KafkaAvroSimpleProducerMain {
+    public static void main(String[] args) {
+        System.out.printf("Main.main");
+    }
+}
