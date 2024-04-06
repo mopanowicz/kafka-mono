c7c4cf28c9a807ed392226ca9837e6644dbaf53d
mopanowicz <marek.opanowicz@gmail.com>
2024-04-06 17:47:18 +0200

    Add logging

--- kafka-avro-simple-consumer/src/main/java/com/example/KafkaAvroSimpleConsumerMain.java
+++ kafka-avro-simple-consumer/src/main/java/com/example/KafkaAvroSimpleConsumerMain.java
@@ -1,7 +1,13 @@
 package com.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class KafkaAvroSimpleConsumerMain {
+
+    static Logger log = LoggerFactory.getLogger(KafkaAvroSimpleConsumerMain.class);
+
     public static void main(String[] args) {
-        System.out.printf("KafkaAvroSimpleConsumerMain.main");
+        log.info("main");
     }
 }
--- kafka-avro-simple-producer/src/main/java/com/example/KafkaAvroSimpleProducerMain.java
+++ kafka-avro-simple-producer/src/main/java/com/example/KafkaAvroSimpleProducerMain.java
@@ -1,7 +1,13 @@
 package com.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class KafkaAvroSimpleProducerMain {
+
+    static Logger log = LoggerFactory.getLogger(KafkaAvroSimpleProducerMain.class);
+
     public static void main(String[] args) {
-        System.out.printf("Main.main");
+        log.info("main");
     }
 }
