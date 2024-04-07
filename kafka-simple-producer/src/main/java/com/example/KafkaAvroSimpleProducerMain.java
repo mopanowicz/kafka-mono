1173b0acdfc4aa69fd9963fc0ffdbd2ac053f111
mopanowicz <marek.opanowicz@gmail.com>
2024-04-07 12:26:51 +0200

    Add kafka producer

--- kafka-simple-producer/src/main/java/com/example/KafkaAvroSimpleProducerMain.java
+++ kafka-simple-producer/src/main/java/com/example/KafkaAvroSimpleProducerMain.java
@@ -1,13 +1,34 @@
 package com.example;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
 
 public class KafkaAvroSimpleProducerMain {
 
     static Logger log = LoggerFactory.getLogger(KafkaAvroSimpleProducerMain.class);
+    static Configuration configuration = Configuration.getInstance();
 
     public static void main(String[] args) {
-        log.info("main");
+        Properties properties = new Properties();
+        properties.putAll(configuration.getAll());
+        KafkaProducer producer = new KafkaProducer(properties);
+
+        ProducerRecord<Object, Object> record = new ProducerRecord<>(
+                "test-messages",
+                "key-"+ (long)(Math.random() * 1000),
+                "value-"+ (long)(Math.random() * 1000));
+        try {
+            log.info("sending {}", record);
+            producer.send(record);
+            producer.flush();
+        } catch(Exception e) {
+            log.error("exception", e);
+        } finally {
+            producer.close();
+        }
     }
 }
