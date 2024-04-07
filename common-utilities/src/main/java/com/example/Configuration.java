74927abeb9829fe77a06ba206e8c81624750f971
mopanowicz <marek.opanowicz@gmail.com>
2024-04-07 12:33:50 +0200

    Add logging

--- common-utilities/src/main/java/com/example/Configuration.java
+++ common-utilities/src/main/java/com/example/Configuration.java
@@ -1,5 +1,8 @@
 package com.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.Map;
@@ -7,18 +10,24 @@ import java.util.Properties;
 
 public class Configuration {
 
+    static Logger log = LoggerFactory.getLogger(Configuration.class);
+
     private final static Configuration instance = new Configuration();
 
     private final Properties configuration = new Properties();
 
     private Configuration() {
-        File file = new File( "configuration.properties");
+        String configurationFile = "configuration.properties";
+        File file = new File(configurationFile);
         if (file.canRead()) {
             try {
                 configuration.load(new FileInputStream(file));
             } catch (Exception e) {
-                e.printStackTrace();
+                log.error("exception", e);
             }
+            log.info("loaded {}", configurationFile);
+        } else {
+            log.warn("configuration file {} not found", configurationFile);
         }
     }
 
