015a65bf584ce009cf7531e64aa604aeae389520
mopanowicz <marek.opanowicz@gmail.com>
2024-04-18 23:07:44 +0200

    Update table creation and notes

--- db/postgres/message.sql
+++ db/postgres/message.sql
@@ -1,6 +1,8 @@
 CREATE TABLE message (
     key character varying(4096) NOT NULL,
-    value character varying(65535)
+    value character varying(65535),
+    received timestamp,
+    saved timestamp DEFAULT NOW()
 );
 
 ALTER TABLE ONLY message ADD CONSTRAINT message_pk PRIMARY KEY (key);
--- db/postgres/notes.md
+++ db/postgres/notes.md
@@ -3,3 +3,5 @@
 ```bash
 psql -U kafkaconnect -W
 ```
+
+https://d1i4a15mxbxib1.cloudfront.net/api/plugins/confluentinc/kafka-connect-jdbc/versions/10.7.6/confluentinc-kafka-connect-jdbc-10.7.6.zip
