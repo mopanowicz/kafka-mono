9a9fe63f86693af88c7a50592aa9bfafc8a789a2
Marek Opanowicz <marek.opanowicz@santander.pl>
2024-04-21 17:15:42 +0200

    Update simple_one table

--- db/postgres/simple_one.sql
+++ db/postgres/simple_one.sql
@@ -2,7 +2,8 @@ CREATE TABLE simple_one (
     id character varying(1024) NOT NULL,
     sent bigint,
     text character varying(65535),
-    created timestamp DEFAULT NOW()
+    saved bigint DEFAULT cast(extract(epoch from (now() at time zone 'utc')) * 1000 as bigint),
+    latency bigint GENERATED ALWAYS AS (saved - sent) STORED
 );
 
 ALTER TABLE ONLY simple_one ADD CONSTRAINT simple_one_pk PRIMARY KEY (id);
