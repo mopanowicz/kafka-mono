ec8d6b58aa830bcd399eeebb24e7af90662352ed
mopanowicz <marek.opanowicz@gmail.com>
2024-05-02 11:02:36 +0200

    Add log4j + load jdbc driver

--- simple-jdbc-informix/src/main/java/com/example/Main.java
+++ simple-jdbc-informix/src/main/java/com/example/Main.java
@@ -1,7 +1,18 @@
 package com.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+
+    static Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("main");
+        log.info("main");
+        try {
+            Class.forName("com.informix.jdbc.IfxDriver");
+        } catch (Exception e) {
+            log.error("Loading of Informix JDBC driver failed", e);
+        }
     }
 }
