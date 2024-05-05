f8c9d184f30e64d6ec4181240ede9d129446deb9
mopanowicz <marek.opanowicz@gmail.com>
2024-05-05 14:41:29 +0200

    Add database tagble population

--- simple-jdbc-informix/src/main/java/com/example/Main.java
+++ simple-jdbc-informix/src/main/java/com/example/Main.java
@@ -1,7 +1,14 @@
 package com.example;
 
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.UUID;
 
 public class Main {
 
@@ -11,8 +18,21 @@ public class Main {
         log.info("main");
         try {
             Class.forName("com.informix.jdbc.IfxDriver");
+            String url = String.format("jdbc:informix-sqli://%s:%d/%s:INFORMIXSERVER=%s",
+                    "informix.virtual.box", 12074,
+                    "kafkaconnect",
+                    "ol_informix1410");
+            String user = "informix";
+            String password = "informix1";
+            try (Connection connection = DriverManager.getConnection(url, user, password)) {
+                PreparedStatement ps = connection.prepareStatement("insert into simple_one (id,sent,text) values (?,?,?)");
+                ps.setString(1, UUID.randomUUID().toString());
+                ps.setLong(2, System.currentTimeMillis());
+                ps.setString(3, RandomStringUtils.random(100, true, true));
+                ps.executeUpdate();
+            }
         } catch (Exception e) {
-            log.error("Loading of Informix JDBC driver failed", e);
+            log.error("Exception", e);
         }
     }
 }
